eb9c1d92aaf877d8929a8476d85b6fb7bbeebd9b
JosebaRuizO <joseba.tic@gmail.com>
2017-04-11 11:43:49 +0200

    asl

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
 });
 
 //listen on port 8080
-webServer.listen(8080, function () {
+webServer.listen(80, function () {
     console.log('listening on http://localhost:8080');
 });
